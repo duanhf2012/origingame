a583f4c1694c0218e5f594cf52cfe7b479ec3ca5
duanhf2012 <6549168@qq.com>
2024-09-20 14:33:12 +0800

    新增luban范例

--- table/gen.sh
+++ table/gen.sh
@@ -5,7 +5,11 @@ LUBAN_DLL=$WORKSPACE/Tools/Luban/Luban.dll
 CONF_ROOT=.
 
 dotnet $LUBAN_DLL \
-    -t all \
+    -t server \
+	-c go-json \
     -d json \
     --conf $CONF_ROOT/luban.conf \
-    -x outputDataDir=output
+	-x outputCodeDir=tabledef \
+    -x outputDataDir=$WORKSPACE/bin/config/dev/datas \
+    -x lubanGoModule=demo/luban
+	
--- table/tabledef/item.Item.go
+++ table/tabledef/item.Item.go
@@ -21,6 +21,8 @@ type ItemItem struct {
     Quality int32
     ExchangeList []*ItemItemExchange
     ExchangeColumn *ItemItemExchange
+    AddAttr map[int32]int64
+    AddItem []*ItemData
 }
 
 const TypeId_ItemItem = 2107285806
@@ -52,6 +54,37 @@ func NewItemItem(_buf map[string]interface{}) (_v *ItemItem, err error) {
                 }
     ; _v.ExchangeList = __x__ }}
     { var _ok_ bool; var __json_exchange_column__ interface{}; if __json_exchange_column__, _ok_ = _buf["exchange_column"]; !_ok_ || __json_exchange_column__ == nil { err = errors.New("exchange_column error"); return } else { var __x__ *ItemItemExchange;  { var _ok_ bool; var _x_ map[string]interface{}; if _x_, _ok_ = __json_exchange_column__.(map[string]interface{}); !_ok_ { err = errors.New("__x__ error"); return }; if __x__, err = NewItemItemExchange(_x_); err != nil { return } }; _v.ExchangeColumn = __x__ }}
+    { var _ok_ bool; var __json_AddAttr__ interface{}; if __json_AddAttr__, _ok_ = _buf["AddAttr"]; !_ok_ || __json_AddAttr__ == nil { err = errors.New("AddAttr error"); return } else { var __x__ map[int32]int64;  {
+                    var _arr0_ []interface{}
+                    var _ok0_ bool
+                    if _arr0_, _ok_ = (__json_AddAttr__).([]interface{}); !_ok_ { err = errors.New("__x__ error"); return }
+    
+                    __x__ = make(map[int32]int64)
+                    
+                    for _, _e0_ := range _arr0_ {
+                        var _kv0_ []interface{}
+                        if _kv0_, _ok0_ = _e0_.([]interface{}); !_ok0_ || len(_kv0_) != 2 { err = errors.New("__x__ error"); return }
+                        var _key0_ int32
+                        { var _ok_ bool; var _x_ float64; if _x_, _ok_ = _kv0_[0].(float64); !_ok_ { err = errors.New("_key0_ error"); return }; _key0_ = int32(_x_) }
+                        var _value0_ int64
+                        { var _ok_ bool; var _x_ float64; if _x_, _ok_ = _kv0_[1].(float64); !_ok_ { err = errors.New("_value0_ error"); return }; _value0_ = int64(_x_) }
+                        __x__[_key0_] = _value0_
+                    }
+                    }; _v.AddAttr = __x__ }}
+    { var _ok_ bool; var __json_AddItem__ interface{}; if __json_AddItem__, _ok_ = _buf["AddItem"]; !_ok_ || __json_AddItem__ == nil { err = errors.New("AddItem error"); return } else { var __x__ []*ItemData;  {
+                    var _arr0_ []interface{}
+                    var _ok0_ bool
+                    if _arr0_, _ok0_ = (__json_AddItem__).([]interface{}); !_ok0_ { err = errors.New("__x__ error"); return }
+    
+                    __x__ = make([]*ItemData, 0, len(_arr0_))
+                    
+                    for _, _e0_ := range _arr0_ {
+                        var _list_v0_ *ItemData
+                        { var _ok_ bool; var _x_ map[string]interface{}; if _x_, _ok_ = _e0_.(map[string]interface{}); !_ok_ { err = errors.New("_list_v0_ error"); return }; if _list_v0_, err = NewItemData(_x_); err != nil { return } }
+                        __x__ = append(__x__, _list_v0_)
+                    }
+                }
+    ; _v.AddItem = __x__ }}
     return
 }
 
